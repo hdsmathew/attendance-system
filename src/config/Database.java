fd7ad133589d625210e5e8b588ee8dcc55cf236b
hdsmathew <chnmathew@gmail.com>
2022-07-01 12:29:25 +0400

    db connection

---
+++ src/config/Database.java
@@ -0,0 +1,28 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+	private static final String driver = "com.mysql.cj.jdbc.Driver";
+	private static final String url = "jdbc:mysql://localhost/attendancesystem";
+	private static final String username = "hdsmathew";
+	private static final String password = "mathewrcpl";
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		
+		try {
+			Class.forName(driver);
+			conn = DriverManager.getConnection(url, username, password);
+			
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return conn;
+	}
+}
